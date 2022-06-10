20b22a9e605bf24704ea5838ab75bea18b270e1b
leah karienye <karienyeleah@gmail.com>
2022-06-10 17:20:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+/*function superbowlWin(record, finder){
+    let result;
+    for (let year of record){
+        if (finder(year)=== true){
+            result = year;
+            break;
+        }
+        return result;
+    }
+    else if (finder())
+}
+*/
+function superbowlWin(collection) {
+    
+    for (const record of collection) {
+      console.log(record.year);
+    }
+  }
+  
+  superbowlWin(record);
